7ec2c12ed75e1f8a5d5815d3b7c765dd1635d797
Vladislav Mazannik <mazanniknnik@gmail.com>
2022-08-11 01:12:14 +0300

    feat: end push

--- src/index.js
+++ src/index.js
@@ -31,10 +31,24 @@ module.exports = function toReadable(number) {
         return arrayThree[+numberToString[0] - 2] + ' ' + arrayOne[+numberToString[1] - 1];
     };
 
-
-    if ((numberToString.length == 3) && (+numberToString[1] == 0) && (+numberToString[2] == 0)) {
-
-        return arrayOne[number - 1] + ' ' + 'hundred';
-
+//100 200 300 ... 900
+    if ((numberToString.length == 3) && (number % 100 == 0)) {
+        return arrayOne[numberToString[0] - 1] + ' ' + 'hundred';
+    };
+//101 102 103... 109... 201 202 203... 209...
+    if ((numberToString.length == 3) && (+numberToString[1] == 0) && (+numberToString[2] != 0)) {
+        return arrayOne[numberToString[0] - 1] + ' ' + 'hundred' + ' ' + arrayOne[numberToString[2] - 1];
+    };
+//110 111 112... 119... 210 211 212... 219
+    if ((numberToString.length == 3) && (+((numberToString[1]) + (numberToString[2])) <=19) && (+numberToString[1] != 0)) {
+        return arrayOne[numberToString[0] - 1] + ' ' + 'hundred' + ' ' + arrayTwo[+((numberToString[1]) + (numberToString[2])) - 10];
+    };
+//120 130 140... 190... 220 230 240
+    if ((numberToString.length == 3) && (+((numberToString[1]) + (numberToString[2])) > 19) && (+((numberToString[1]) + (numberToString[2])) %10 == 0)) {
+        return arrayOne[numberToString[0] - 1] + ' ' + 'hundred' + ' ' + arrayThree[+numberToString[1] - 2];
+    };
+//121 122 123.. 199
+    if ((numberToString.length == 3) && (+((numberToString[1]) + (numberToString[2])) > 19) && (+((numberToString[1]) + (numberToString[2])) %10 != 0)) {
+        return arrayOne[numberToString[0] - 1] + ' ' + 'hundred' + ' ' + arrayThree[+numberToString[1] - 2] + ' ' + arrayOne[+numberToString[2] - 1];
     };
 };
